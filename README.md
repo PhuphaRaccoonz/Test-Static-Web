f6457364d3c7bfdaeb300c4fcfd99a624c4b7982
PhuphaRaccoonz <phupha.thawon2547@gmail.com>
2025-08-10 21:52:56 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+Phupha Thawonruangrit 662110162
+GitHub Link : https://github.com/PhuphaRaccoonz/Test-Static-Web.git
---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactBtn").addEventListener("click", function() {
+    alert("You can contact me at: phupha.thawon2547@gmail.com");
+    alert("Raccoon will run the world!");
+});
